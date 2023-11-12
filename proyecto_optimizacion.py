d03b39fe27d5eafbbbcefe24721bd37e15d7fe26
JackRakham <j.villatet@uniandes.edu.co>
2023-11-12 10:28:18 -0500

    Funcionalidad del proyecto

--- proyecto_optimizacion.py
+++ proyecto_optimizacion.py
@@ -10,11 +10,11 @@ M = ConcreteModel()
 Familias = RangeSet(1,5)
 M.Familias = Familias
 #set para identificar a cada centro
-Centros = RangeSer(1,2)
-M.Centros = Centros
+Centros = RangeSet(1,2)
+M.Centros = RangeSet(1,2)
 
 #PARAMETROS
-familias_data = {1:4, 2:3, 3:5, 2:1}
+familias_data = {1:4, 2:3, 3:5, 4:1,5:3}
 #Parametro que dice cuantos miembros tiene cada familia
 M.Miembros_Familias = Param(M.Familias, within=NonNegativeReals, initialize=familias_data)
 
@@ -25,9 +25,9 @@ CantidadCentros = len(Centros)
 #Indica a que centro fue asignada cada familia
 M.Asignaciones = Var(M.Familias,M.Centros,domain=Binary)
 #Indica cuantos ocupantes tiene cada centro
-M.Ocupantes_x_Centro = Var(M.Centros,Domain=Integers)
+M.Ocupantes_x_Centro = Var(M.Centros,domain=Integers)
 #Indica el promedio actual de ocupantes en los centros de refugiados
-M.promedio = Var(Domain=Reals)
+M.promedio = Var(domain=Reals)
 
 #OBJETIVO
 #Suma la diferencia de la cantidad de ocupantes de cada miembro con el promedio de ocupantes,
@@ -36,3 +36,19 @@ M.obj = Objective(expr= sum( abs(M.Ocupantes_x_Centro[j] - M.promedio) for j in 
 
 
 #ECUACIONES
+#Esta restriccion se utiliza para recalcular el promedio
+M.res1 = Constraint(expr= sum(M.Ocupantes_x_Centro[j]/(CantidadCentros+0.1) for j in M.Centros)==M.promedio)
+
+#Esta restricción se usa para asegurar que cada familia esta asignada a un centro de refugiados. 
+def Asignado_rule(Model, i):
+    return sum(M.Asignaciones[i,j] for j in M.Centros) == 1
+M.Asignado = Constraint(M.Familias,rule= Asignado_rule)
+
+#Esta restricción se utiliza para asegurar que la ocupación del centro de refugiados sea equivalente a la suma de la cantidad de miembros de las familias que lo ocupan. 
+def Coherencia_rule(Model,j):
+    return sum(M.Asignaciones[i,j]*M.Miembros_Familias[i] for i in M.Familias)==M.Ocupantes_x_Centro[j]
+M.Coherencia = Constraint(M.Centros, rule=Coherencia_rule)
+
+SolverFactory('mindtpy').solve(M,mip_solver='glpk',nlp_solver='ipopt')
+
+M.display()
